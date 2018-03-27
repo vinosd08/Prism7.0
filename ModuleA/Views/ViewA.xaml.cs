3458722b8c8cce132bee8e8785ee6aa494fb1dbd
vinoth <vinoth@pointelsolutions.com>
2018-03-27 22:35:24 +0530

    added code

--- ModuleA/Views/ViewA.xaml.cs
+++ ModuleA/Views/ViewA.xaml.cs
@@ -13,14 +13,14 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace MainModule
+namespace ModuleA.Views
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for ViewA.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class ViewA : UserControl
     {
-        public MainWindow()
+        public ViewA()
         {
             InitializeComponent();
         }
---
+++ Modules/App.xaml.cs
@@ -0,0 +1,31 @@
+﻿
+using System;
+using System.Windows;
+using Prism.Ioc;
+using Prism.Unity;
+using Prism.Modularity;
+using Modules.Views;
+
+namespace Modules
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    internal partial class App : PrismApplication
+    {
+        protected override Window CreateShell()
+        {
+            return Container.Resolve<MainWindow>();
+        }
+        protected override void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+            //register class object here
+            //containerRegistry.RegisterInstance<ShellView>(_tempShellView);
+        }
+        protected override IModuleCatalog CreateModuleCatalog()
+        {
+            return new ConfigurationModuleCatalog();
+            //return  new DirectoryModuleCatalog() {  ModulePath = Environment.CurrentDirectory };
+        }
+    }
+}
